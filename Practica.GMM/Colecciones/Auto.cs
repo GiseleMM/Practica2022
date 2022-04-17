be5240854fd7d36ea611de6524d81353b61a8763
gisele <giselemedina01@gmail.com>
2022-04-16 23:11:24 -0300

    Arrays , MAtrices, Array de arrays,Collecciones genericas no genericas,Lista

---
+++ Practica.GMM/Colecciones/Auto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones
+{
+    public class Auto
+    {
+        private string patente;
+        private string marca;
+
+        public Auto(string patente, string marca)
+        {
+            this.patente = patente;
+            this.marca = marca;
+        }
+        public string MostrarAuto()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"patente: {this.patente}");
+            sb.AppendLine($"marca:{this.marca}");
+            return sb.ToString();
+         
+        }
+    }
+}
---
+++ Practica.GMM/Colecciones/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Colecciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //            -------------------------------COLECCIONES------------------------------------------------------
+            //Las colecciones son objetos especializados en almacenar, organizar y administrar gran cantidad de datos.
+            //Características tamaño dinámico, implementación IEnumerables, copiar su contenido a un array(base 0), indexación base cero
+            //Matrices.
+
+
+ /*ARRAYS*/
+            int[] numeros;
+            numeros = new int[4];
+
+            numeros[0] = 1;
+            numeros[1] = 33;
+            numeros[2] = 12;
+            numeros[3] = 91;
+            /*recorrer el array*/
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine($"indice: {i} valor: { numeros[i]}");
+            }
+            /*recorro con el foreach */
+            foreach (int item in numeros)
+            {
+                //recorro  sin importar el indice
+                // puedo recorrer objetos
+                Console.WriteLine(item);
+            }
+
+            Auto[] autos = new Auto[3];//creo la lista de autos
+            autos[0] = new Auto("2323", "dsada");// creo cada objeto auto
+            autos[1] = new Auto("6666", "SSade");
+            autos[2] = new Auto("8555", "owdu");// si pongo[7] rompe el foreach
+
+
+
+
+            foreach (Auto item in autos) // cambiar el var de tab por defecto
+            {//recorro pero si no hay nada comoes un tipo de refrencia
+                // tiene  null y si llamamos algun metodo rompe
+                if (item is not null)
+                {
+                    Console.WriteLine(item.MostrarAuto());
+                }
+            }
+
+ /*MATRICES*/
+            int[,] matriz = new int[3, 2] { { 1, 2 }, { 2, 4 }, { 2, 5 } };
+            foreach (int item in matriz)
+            {
+                Console.WriteLine(item);//esto muestra todos los elementos
+            }
+ /*ARRAY DE ARRAY */
+            // array donde cada elemento es un array
+            int[][] matricesMultidimensionales = new int[3][];//array de 3 filas
+            matricesMultidimensionales[0] = new int[] { 1, 3 };
+            matricesMultidimensionales[1] = new int[] { 5, 43, 2, 4 };
+            matricesMultidimensionales[2] = new int[] { 7, 2 };
+            //puedo acceder a un elemnto indicando indice
+            Console.WriteLine(matricesMultidimensionales[0][0]);
+
+
+ /*COLECCIONES GENERICAS y NO GENERICAS*/
+            //las NO genericas no cientas con seguridad de tipo
+            //las genericas todos los elementos de la coleccion tienen el mismo tipo de datos
+            
+            
+            /*         LIstas           */
+            List<string> nombres = new List<string>(); // entre<> el tipo de los elemnetos de la colleccion
+            //todas las colleciones son objetos y hay q instansiarlos en base cero
+           
+            //AGREGAR
+            nombres.Add("lautaro");// cree una nueva  posicion en la lista con  metodo Add
+            nombres.Add("Leonel");
+            nombres.Add("lali");
+            nombres[1] = "Rosa";// puedo sobreescribir con el indices que cree con Add pero si intento ingresar nombres[10]="hiid" NO Funciona por que ese indice no existe
+
+            foreach (string item in nombres)
+            {
+                Console.WriteLine( item );
+            }
+
+            //REMOVER
+            Console.WriteLine("-----lista.Remove(string)-----");// borra el 1 string
+            nombres.Remove("Rosa");
+            foreach (string item in nombres)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----removeAt(indice)-----"  );
+            nombres.RemoveAt(1);//remuevo indice especificado
+            foreach (string item in nombres)
+            {
+                Console.WriteLine(item);
+            }
+
+ /*COLECCIONES CONCURRESNTES e INMUTABLES*/
+            //concurrencia proporcionans operacionees eficientes y seguras para acceder a los elemntos 
+            // de la coleccion desde varios hilos de ejecucion
+            // con concurrente nos referismos con 2 cosas q estan sucediendo al mismo tiempo
+            // inmutables no pueden ser  modificadas, la  misma cantidad ni ningun elemento nuevo
+
+
+        }
+
+
+    }
+}
