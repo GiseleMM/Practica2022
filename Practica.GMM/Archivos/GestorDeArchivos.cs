099c4a2ffc1a651f786cbfee4bc09c7b16ac62f2
gisele <giselemedina01@gmail.com>
2022-05-26 20:57:11 -0300

    Escribir archivo con StreamWrite

---
+++ Practica.GMM/Archivos/GestorDeArchivos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos
+{
+    public static class GestorDeArchivos
+    {
+        static string rutaBaseEscritorio;
+       static GestorDeArchivos()
+        {
+            GestorDeArchivos.rutaBaseEscritorio = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        }
+        public static bool Escribir(string path, string contenido, bool append)//append determina si concateno(true) o sobreescribo(false)
+        {
+            bool seEscribio = false;
+            StreamWriter sw = null;
+            try
+            {
+                string rutaCompleta = $"{GestorDeArchivos.rutaBaseEscritorio}\\{path}";
+                sw = new StreamWriter(rutaCompleta, append);
+                sw.WriteLine(contenido);
+                seEscribio = true;
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if(sw!=null)// si es distinto de null cierro archivo
+                {
+                    sw.Close();
+                }
+            }
+            return seEscribio;
+        }
+
+
+    }
+}
---
+++ Practica.GMM/Archivos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Archivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribir en archivos");
+            GestorDeArchivos.Escribir("miArchivoPrueba.txt", "odio archivo", true);// true concatena si existe
+            //GestorDeArchivos.Escribir("miArchivoPrueba.txt", "odio archivo 2", false); //sobreescribe si existe
+        }
+    }
+}
